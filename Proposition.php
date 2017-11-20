f8b3067d11598c91bd4732c9a8770db8a408d02f
gaelborde <32923751+gaelborde@users.noreply.github.com>
2017-11-20 11:02:40 +0100

    Update Proposition.php

--- Proposition.php
+++ Proposition.php
@@ -5,47 +5,26 @@
 //Et ensuite de mettre ce petit tableau contenant l'ID dans un grand tableaux qui contiendra tous les IDs.
 function Tableau ($donnees, $x) {
 global $user;
-  //Je pars dans l'idée que "$donnees" sera la variable qui sélectionnera la l'ID par la suite.
-    // for ($i=0; $i < count($donnees); $i++) {
-      $user[$x] = str_split($donnees); //on met l'ID dans un tableau pour l'analyser facilement
-
-      print_r($user[$x]);
-    // }
-    // foreach ($interet as $int) {
-    //   echo '<p>'.$int[$x].'</p>';
-    // }
+    //Je pars dans l'idée que "$donnees" sera la variable qui sélectionnera la l'ID par la suite.
+    $user[$x] = str_split($donnees); //on met l'ID dans un tableau pour l'analyser facilement
 
     array_splice($user[$x], 0, 2); //on enlève les deux premier element de l'ID car ils ne font pas partie de l'ID donnees
-    for ($m=0; $m < count($user[$x]); $m++) {
-      echo "<br>element de user deuxieme degre : ".$user[$x][$m];
-    }
-    echo "<br>azer";
-    // array_push($user[$x], $interet);
-    print_r($user);
-    echo "<br>azerty";
+
     return $user;
 
 }
-
-echo "<br>";
 
 // Fonction qui permet de comparer les ID des différentes personnes et de faire un tableau de résultat.
 function Traitement()
 {
   global $user;
-  echo "<br>rentré dans traitement";
   $compteur = array(1 => 0);
-  echo "<br>4";
 	for ($i=1; $i < 2 ; $i++) {
-    echo "<br>az  :".count($user[0]);
 		for ($j=0; $j < count($user[$i]) ; $j++) {
-      echo "<br>6";
 			if ($user[0][$j] + $user[$i][$j] == 2){
-        echo "<br>7";
 				$compteur[$i] +=1;
 			}
 		}
-    print_r($compteur);
 	}
   return $compteur;
 }
@@ -61,7 +40,8 @@ function Resultat()
 		}
 	}
 }
-	//$interet = array('');
+
+
 	$user = array('');
 	// $donnees = $bdd->query('SELECT Id_utilisateur FROM Utilisateur');
   //   $compt = 0;
@@ -80,9 +60,7 @@ for ($x=0; $x < 2 ; $x++) {
   }
   Tableau($donnees,$x);
 }
-echo "<br>2";
 print_r($user);
 $compteur = Traitement();
 Resultat($compteur);
-echo "<br>3";
  ?>
