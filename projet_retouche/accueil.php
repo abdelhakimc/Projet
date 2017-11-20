0223f692de9733265174b4018440ec693f1f58ee
pierreipi <32924071+pierreipi@users.noreply.github.com>
2017-11-20 17:00:40 +0100

    HTML_by_Gaël

---
+++ projet_retouche/accueil.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+
+<link rel="stylesheet" type="text/css" href="site.css" media="all" />
+<link rel="stylesheet" href="./css/bootstrap.min.css"/>
+      <link rel="stylesheet" href="./css/main.css"/>
+      <script type="application/javascript" src="./js/jquery-2.1.1.min.js"></script>
+      <script type="application/javascript" src="./js/bootstrap.min.js"></script>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+
+</script>
+<html>
+
+  <head>
+
+    <meta charset="utf-8"/>
+    <title>Ma page</title>
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    <style>body{font-family: 'Raleway',Comic Sans MS;}</style>
+
+
+  </head>
+
+
+
+  <body class="accueil">
+  <div class="container">
+    <div class="col-md-6">
+
+      <h1><b><MARQUEE scrollAmount="12" DIRECTION="LEFT">Bienvenue Sur Le Réseau U'DEV !</MARQUEE></b></h1>
+      <img class="col-10" id="img"  src="cgii.jpg">
+
+      <a href="file:///C:/Users/etudiant31/Desktop/PROJET/site/site2.html" class="button">Inscription</a>
+
+    </div>
+  <nav>
+      <ul id="menu">
+          <li><a href="#">PRESENTATION</a>
+             <ul>
+                <li><a href="#">RESEAU PROFESSIONNELLE</a></li>
+                <li><a href="#">RESEAU PERSONNELLE</a></li>
+             </ul>
+          </li>
+           <li><a href="#">U'DEV</a>
+             <ul>
+                <li><a href="#">CLERMONT-FERRAND</a></li>
+                <li><a href="#">FRANCE</a></li>
+             </ul>
+          </li>
+          <li><a href="#">CONTACTS</a>
+             <ul>
+                <li><a href="#">SUGGESTIONS</a></li>
+             </ul>
+          </li>
+       </ul>
+  </nav>
+
+  </div>
+
+  </body>
+
+  <footer>
+
+  </footer>
+
+</html>
---
+++ projet_retouche/inscription.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<link rel="stylesheet" type="text/css" href="site.css" media="all" />
+<link rel="stylesheet" href="./css/bootstrap.min.css"/>
+      <link rel="stylesheet" href="./css/main.css"/>
+      <script type="application/javascript" src="./js/jquery-2.1.1.min.js"></script>
+      <script type="application/javascript" src="./js/bootstrap.min.js"></script>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <title>Inscription</title>
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    </style>
+  </head>
+
+
+  <body>
+    <div class="container text-center">
+      <div class="row divrow">
+        <form action="" method="post">
+
+          <div class="col-xs-12 col-sm-4 col-md-5 col-xl-5 boxConnexion">
+            <p class="inscription">Nom : <input class="inputinscription" type="text" name="" value=""></p>
+            <p class="inscription">Prenom : <input class="inputinscription" type="text" name="" value=""></p>
+            <p class="inscription">Mail : <input class="inputinscription" type="text" name="" value=""></p>
+            <p class="inscription">Campus : <input class="inputinscription" type="text" name="" value=""></p>
+            <p class="inscription">Ville : <input class="inputinscription" type="text" name="" value=""></p>
+            <p class="inscription">Age : <input class="inputinscription" type="text" name="" value=""></p>
+            <p class="inscription">Genre : <input class="inputinscription" type="text" name="" value=""></p>
+          </div>
+
+
+          <div class="col-xs-12 col-sm-4 col-md-5 col-xl-5 boxConnexion">
+            <h1>Compétences</h1>
+                  <div class="row">
+
+<!-- formulaire de compétence à sélectionner -->
+                    <div class="col-xs-8 col-sm-8 col-md-8 col-xl-8">
+                        <div class="input-group divinscription">
+                          <span class="input-group-addon">
+                            <input type="checkbox" aria-label="...">
+                          </span>
+                          <label class=""> HTML/CSS/JS </label>
+
+                        </div>
+                        <div class="input-group divinscription">
+                          <span class="input-group-addon">
+                            <input type="checkbox" aria-label="...">
+                          </span>
+                          <label class=""> Java </label>
+
+                        </div>
+                        <div class="input-group divinscription">
+                          <span class="input-group-addon">
+                            <input type="checkbox" aria-label="...">
+                          </span>
+                          <label class=""> SQL </label>
+
+                        </div>
+                        <div class="input-group divinscription">
+                          <span class="input-group-addon">
+                            <input type="checkbox" aria-label="...">
+                          </span>
+                          <label class=""> Android </label>
+
+                        </div>
+                        <div class="input-group divinscription">
+                          <span class="input-group-addon">
+                            <input type="checkbox" aria-label="...">
+                          </span>
+                          <label class=""> C#/C++ </label>
+                        </div>
+
+                        <div class="input-group divinscription">
+                          <span class="input-group-addon">
+                            <input type="checkbox" aria-label="...">
+                          </span>
+                          <label class=""> Python </label>
+
+                        </div>
+                      </div>
+
+
+                </div>
+          </div>
+
+          <div class="col-xs-12 col-sm-4 col-md-5 col-xl-5 boxConnexion">
+            <h1>Centres d'intéret</h1>
+
+            <div class="row">
+
+<!-- formulaire de loisirs à sélectionner -->
+              <div class="col-xs-8 col-sm-8 col-md-8 col-xl-8">
+                    <div class="input-group divinscription">
+                      <span class="input-group-addon">
+                        <input type="checkbox" aria-label="...">
+                      </span>
+                      <label class=""> Litterature </label>
+
+                    </div>
+                    <div class="input-group divinscription">
+                      <span class="input-group-addon">
+                        <input type="checkbox" aria-label="...">
+                      </span>
+                      <label class=""> Sport </label>
+
+                    </div>
+                    <div class="input-group divinscription">
+                      <span class="input-group-addon">
+                        <input type="checkbox" aria-label="...">
+                      </span>
+                      <label class=""> Cuisine </label>
+
+                    </div>
+                    <div class="input-group divinscription">
+                      <span class="input-group-addon">
+                        <input type="checkbox" aria-label="...">
+                      </span>
+                      <label class=""> Jeux vidéo </label>
+
+                    </div>
+                    <div class="input-group divinscription">
+                      <span class="input-group-addon">
+                        <input type="checkbox" aria-label="...">
+                      </span>
+                      <label class=""> Cinéma </label>
+                    </div>
+
+                    <div class="input-group divinscription">
+                      <span class="input-group-addon">
+                        <input type="checkbox" aria-label="...">
+                      </span>
+                      <label class=""> Musique </label>
+                    </div>
+
+                    <div class="input-group divinscription">
+                      <span class="input-group-addon">
+                        <input type="checkbox" aria-label="...">
+                      </span>
+                      <label class=""> Nouvelle-techno </label>
+                    </div>
+
+                    <div class="input-group divinscription">
+                      <span class="input-group-addon">
+                        <input type="checkbox" aria-label="...">
+                      </span>
+                      <label class=""> Auto/moto </label>
+                    </div>
+
+                    <div class="input-group divinscription">
+                      <span class="input-group-addon">
+                        <input type="checkbox" aria-label="...">
+                      </span>
+                      <label class=""> Politique </label>
+                    </div>
+
+                  </div>
+
+
+            </div>
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </body>
+
+  <footer>
+  </footer>
+</html>
