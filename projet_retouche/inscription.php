ecbc52d34ce839e80f825bf899fe991e9e9ca1a1
gaelborde <32923751+gaelborde@users.noreply.github.com>
2017-11-21 11:35:32 +0100

    Add files via upload

---
+++ projet_retouche/inscription.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<link rel="stylesheet" type="text/css" href="site.css" media="all" />
+<link rel="stylesheet" href="./css/bootstrap.min.css"/>
+      <link rel="stylesheet" href="./css/main.css"/>
+      <script type="application/javascript" src="./js/jquery-2.1.1.min.js"></script>
+      <script type="application/javascript" src="./js/bootstrap.min.js"></script>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <title>Inscription</title>
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    </style>
+  </head>
+
+
+  <body>
+    <div class="container">
+        <form action="" method="post">
+          <div class="row text-center formulaire">
+
+<!-- Formulaire à remplir pour avoir quelques informations sur la personne -->
+          <div class="col-xs-11 col-sm-11 col-md-11 col-xl-11">
+            <p class="inscription">Nom : <input class="inputinscription" type="text" name="" value=""></p>
+            <p class="inscription">Prenom : <input class="inputinscription" type="text" name="" value=""></p>
+            <p class="inscription">Mail : <input class="inputinscription" type="email" name="" value=""></p>
+            <p class="inscription">Campus : <input class="inputinscription" type="text" name="" value=""></p>
+            <p class="inscription">Ville : <input class="inputinscription" type="text" name="" value=""></p>
+            <p class="inscription">Age : <input class="inputinscription" type="text" name="" value=""></p>
+            <p class="inscription">Genre : <input class="inputinscription" type="text" name="" value=""></p>
+          </div>
+      </div>
+
+          <div class="row text-center">
+
+<!-- formulaire de compétence à sélectionner -->
+                    <div class="col-xs-12 col-sm-11 col-md-5 col-xl-5 boxFormulaire">
+                      <h1 class="h1formulaire">Compétences</h1>
+
+                        <div class="input-group divinscription">
+                          <span class="input-group-addon">
+                            <input type="checkbox" aria-label="...">
+                          </span>
+                          <label class=""> HTML/CSS/JS </label>
+                        </div>
+
+                        <div class="input-group divinscription">
+                          <span class="input-group-addon">
+                            <input type="checkbox" aria-label="...">
+                          </span>
+                          <label class=""> Java </label>
+                        </div>
+
+                        <div class="input-group divinscription">
+                          <span class="input-group-addon">
+                            <input type="checkbox" aria-label="...">
+                          </span>
+                          <label class=""> SQL </label>
+                        </div>
+
+                        <div class="input-group divinscription">
+                          <span class="input-group-addon">
+                            <input type="checkbox" aria-label="...">
+                          </span>
+                          <label class=""> Android </label>
+                        </div>
+
+                        <div class="input-group divinscription">
+                          <span class="input-group-addon">
+                            <input type="checkbox" aria-label="...">
+                          </span>
+                          <label class=""> C#/C++ </label>
+                        </div>
+
+                        <div class="input-group divinscription">
+                          <span class="input-group-addon">
+                            <input type="checkbox" aria-label="...">
+                          </span>
+                          <label class=""> Python </label>
+                        </div>
+
+                      </div>
+
+
+
+
+
+<!-- formulaire de loisirs à sélectionner -->
+              <div class="col-xs-11 col-sm-11 col-md-6 col-xl-6 boxFormulaire">
+                <h1 class="h1formulaire">Centres d'intéret</h1>
+
+                    <div class="input-group divinscription">
+                      <span class="input-group-addon">
+                        <input type="checkbox" aria-label="...">
+                      </span>
+                      <label class=""> Litterature </label>
+                    </div>
+
+                    <div class="input-group divinscription">
+                      <span class="input-group-addon">
+                        <input type="checkbox" aria-label="...">
+                      </span>
+                      <label class=""> Sport </label>
+                    </div>
+
+                    <div class="input-group divinscription">
+                      <span class="input-group-addon">
+                        <input type="checkbox" aria-label="...">
+                      </span>
+                      <label class=""> Cuisine </label>
+                    </div>
+
+                    <div class="input-group divinscription">
+                      <span class="input-group-addon">
+                        <input type="checkbox" aria-label="...">
+                      </span>
+                      <label class=""> Jeux vidéo </label>
+                    </div>
+
+                    <div class="input-group divinscription">
+                      <span class="input-group-addon">
+                        <input type="checkbox" aria-label="...">
+                      </span>
+                      <label class=""> Cinéma </label>
+                    </div>
+
+                    <div class="input-group divinscription">
+                      <span class="input-group-addon">
+                        <input type="checkbox" aria-label="...">
+                      </span>
+                      <label class=""> Musique </label>
+                    </div>
+
+                    <div class="input-group divinscription">
+                      <span class="input-group-addon">
+                        <input type="checkbox" aria-label="...">
+                      </span>
+                      <label class=""> Nouvelle-techno </label>
+                    </div>
+
+                    <div class="input-group divinscription">
+                      <span class="input-group-addon">
+                        <input type="checkbox" aria-label="...">
+                      </span>
+                      <label class=""> Auto/moto </label>
+                    </div>
+
+                    <div class="input-group divinscription">
+                      <span class="input-group-addon">
+                        <input type="checkbox" aria-label="...">
+                      </span>
+                      <label class=""> Politique </label>
+                    </div>
+
+                </div>
+
+
+            </div>
+
+      </div>
+    </div>
+    <div class="row text-center">
+
+<!-- Condition d'utilisation à cocher -->
+      <div class="col-xs-6 col-sm-6 col-md-6 col-xl-6 condition">
+        <label> J'accèpte les conditions d'utilisation et les mention légale. </label>
+          <input type="checkbox" aria-label="..." value="">
+      </div>
+
+<!-- Bouton d'inscription -->
+      <div class="col-xs-6 col-sm-6 col-md-6 col-xl-6">
+        <button type="button" class="btn btn-default" name="button">S'inscrire</button>
+      </div>
+
+    </div>
+  </form>
+  </body>
+
+  <footer>
+  </footer>
+</html>
