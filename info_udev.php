df21bd7dc24f4795c4a2d24f31a45b0cb9431d9e
DESKTOP-TCNMA0U\etudiant06 <bordegael@hotmail.fr>
2017-11-21 14:18:01 +0100

    gael: page informations modifiée

---
+++ info_udev.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<link rel="stylesheet" type="text/css" href="site.css" media="all" />
+<link rel="stylesheet" href="./css/bootstrap.min.css"/>
+      <link rel="stylesheet" href="./css/main.css"/>
+      <script type="application/javascript" src="./js/jquery-2.1.1.min.js"></script>
+      <script type="application/javascript" src="./js/bootstrap.min.js"></script>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <title>Informations</title>
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    </style>
+  </head>
+
+
+  <body>
+    <div class="container-fluid">
+
+      <div class="row">
+        <div class="col-12 imginfo">
+          <img id="img"  class="img-responsive"  src="cgiii.jpg">
+        </div>
+      </div>
+
+    </div>
+
+  <div class="container" >
+    <div class="row infocgi">
+
+
+      <h1 class="text-center">PRESENTATION</h1>
+
+        <p><a href="https://www.cgi.fr/">CGI</a> crée U'DEV, l'Ecole du Développeur qui accueillera dès le mois de septembre 180 bacheliers sur 6 campus en France (Bordeaux, Clermont-Ferrand, Lille, Lyon, Paris, Toulouse).
+          U'DEV offre l’opportunité d’accéder à une formation de qualité, reconnue sur le marché qui permet de développer des compétences, à la fois sur le plan technique et sur des connaissances métiers. <a href="https://www.cgi.fr/">CGI</a> souhaite redonner ses lettres de noblesse au métier de développeur.
+          La formation est entièrement financée par CGI. A la clé, une opportunité de devenir Développeur logiciels, Concepteur développeur d’applications, Analyste, Consultant nouvelles technologies…
+          CGI a noué une coopération avec deux écoles reconnues en matière de formation au numérique : <a href="http://www.ipi-ecoles.com/">IPI</a>, l'Ecole de référence spécialiste des parcours individualisés et certificateur de compétences et <a href="http://www.epsi.fr/">EPSI</a>, réseau national,
+          reconnu pour son approche pédagogique académique et ses diplômes reconnus par les professionnels et le marché.</p>
+
+    </div>
+
+      <h1 class="text-center infocgi">FORMATION</h1>
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-11 tableau">
+
+
+              <table class="table">
+                <thead>
+<!-- titre de la première partie du tableau -->
+                  <tr>
+                    <th><h3>Programme de formation</h3></th>
+                    <th><h3>Compétences visées</h3></th>
+                  </tr>
+                </thead>
+<!-- Contenue de la première partie -->
+                <tbody>
+                  <tr>
+
+                    <td>
+                      <li>Développer des composants d’interface</li>
+                      <li>Développer la persistance des données</li>
+                      <li>Développer une application n-tiers</li>
+                      <li>Systèmes et réseaux</li>
+                      <li>Langages HTML5/CSS3 – Javascript/Jquery/Angular Js – PHP/Symfony2 – Java/C#/.Net</li>
+                      <li>Compétences transverses : Culture des métiers du numérique, mathématiques appliquées à l’informatique, relations clients/utilisateurs,
+                        environnement économique et juridiques, anglais professionnel.</li>
+                    </td>
+
+                    <td>
+                      <li>Concevoir et développer des applications</li>
+                      <li>Analyser un besoin, un cahier des charges</li>
+                      <li>Développer des applications WEB, mobilité</li>
+                      <li>Développer en langage objet</li>
+                      <li>Développer des composants métier</li>
+                      <li>Concevoir et modéliser une base de données</li>
+                      <li>Maquetter une application</li>
+                      <li>Assurer la maintenance corrective et évolutive de l’application</li>
+                      <li>Préparer et exécuter des plans de tests, recettes</li>
+                      <li>Déployer une application</li>
+                      <li>Assurer le support aux utilisateurs</li>
+                      <li>Gérer un projet informatique (Agile)</li>
+                      <li>Utiliser l’anglais professionnel</li>
+                      <li>Appréhender les fondamentaux systèmes et réseaux</li>
+                    </td>
+
+                  </tr>
+                </tbody>
+                <tr>
+                  <th><h3>Débouchés</h3></th>
+                  <th><h3>Les + de la Formation</h3></th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+
+                  <td>
+                    <li>Développeur logiciels</li>
+                    <li>Concepteur développeur d’applications</li>
+                    <li>Analyste</li>
+                    <li>Consultant nouvelles technologies…</li>
+                    <li>Un programme dédié pour CGI</li>
+                    <li>Une formation complète gratuite et rémunérée</li>
+                    <li>Un emploi garanti chez CGI en alternance (3 ans) puis embauche en CDI chez CGI</li>
+                    <li>180 places à pourvoir sur 6 campus en France</li>
+                    <li>Une formation en phase avec la demande des entreprises</li>
+                    <li>Un parcours de formation sécurisé avec un emploi à la clé</li>
+                  </td>
+
+                  <td>
+                    <li>Un programme dédié pour CGI</li>
+                    <li>Une formation complète gratuite et rémunérée</li>
+                    <li>Un emploi garanti chez CGI en alternance (3 ans) puis embauche en CDI chez CGI</li>
+                    <li>180 places à pourvoir sur 6 campus en France</li>
+                    <li>Une formation en phase avec la demande des entreprises</li>
+                    <li>Un parcours de formation sécurisé avec un emploi à la clé</li>
+                  </td>
+
+                </tr>
+              </tbody>
+              </table>
+            </div>
+        </div>
+      </div>
+
+      <div class="row infocgi">
+        <h1 class="text-center">LE MÉTIER</h1>
+
+            <p>    Le <a href="http://www.onisep.fr/Ressources/Univers-Formation/Formations/Post-bac/Concepteur-developpeur-informatique">Concepteur Développeur Informatique</a> participe, sous la direction d’un chef de projet, à la réalisation d’applications informatiques.
+              En fonction de l’analyse des besoins des utilisateurs et de l’étude fonctionnelle, le concepteur développeur informatique va participer à la réalisation de la phase de spécification détaillée, et implémenter la solution avec les technologies retenues, tout en respectant délais et normes de qualité définis (Phases de conception, de développement/programmation, de tests et recettes, de rédaction de documents techniques, de maintenance corrective et évolutive).
+            </p>
+
+      </div>
+    </div>
+
+  <footer>
+
+  </footer>
+</body>
+</html>
