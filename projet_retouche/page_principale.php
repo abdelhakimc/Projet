9ee42c907c3d0f3dd8db8688815faa20431d3ec7
DESKTOP-TCNMA0U\etudiant06 <bordegael@hotmail.fr>
2017-11-22 12:54:43 +0100

    Page principale

---
+++ projet_retouche/page_principale.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<link rel="stylesheet" type="text/css" href="site.css" media="all" />
+<link rel="stylesheet" href="./css/bootstrap.min.css"/>
+      <link rel="stylesheet" href="./css/main.css"/>
+      <script type="application/javascript" src="./js/jquery-2.1.1.min.js"></script>
+      <script type="application/javascript" src="./js/bootstrap.min.js"></script>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <title>Réseau U'DEV</title>
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+    </style>
+  </head>
+
+
+  <body>
+    <div class="container-fluid">
+      <div class="row hautpage">
+        <div class="col-xs-2 col-md-2 col-lx-2 col-sm-2 photo">
+          <img src="icone.jpg" class="img-responsive logo" alt="">
+        </div>
+
+        <div class="col-xs-10 col-md-9 col-lx-9 col-sm-9">
+          <h1 class="text-center">Réseau U'DEV !</h1>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="container-fluid">
+<!-- Partie profile de la page -->
+    <div class="row col-xs-3 col-md-3 col-lx-3 col-sm-3">
+        <div class="boxProfile">
+<!-- image -->
+          <div class="">
+          <img src="" alt="">
+          </div>
+<!-- tableau contenant les infos sur la personne -->
+          <div class="table-responsive tabprofile">
+           <table class="table">
+
+             <thead>
+               <tr>
+                 <th><h5 class="infoperso">Nom</h5></th>
+               </tr>
+            </thead>
+            <tbody>
+             <td>Borde</td>
+             </tbody>
+
+              <thead>
+               <tr>
+                 <th><h5 class="infoperso">Prénom</h5></th>
+               </tr>
+              </thead>
+              <tbody>
+               <td>Gael</td>
+              </tbody>
+
+             <thead>
+               <tr>
+                 <th><h5 class="infoperso">Age</h5></th>
+               </tr>
+             </thead>
+             <tbody>
+                <td>21 ans</td>
+             </tbody>
+
+             <thead>
+              <tr>
+                <th><h5 class="infoperso">Campus</h5></th>
+              </tr>
+             </thead>
+             <tbody>
+              <td>Clermont-ferrand</td>
+             </tbody>
+
+             <thead>
+              <tr>
+                <th><h5 class="infoperso">Ville</h5></th>
+              </tr>
+             </thead>
+             <tbody>
+              <td>Clermont-ferrand</td>
+             </tbody>
+
+           </table>
+          </div>
+
+
+        </div>
+      </div>
+
+<!-- Partie proposition de la page -->
+      <div class="row">
+        <div class=" col-xs-8 col-md-8 col-lx-8 col-sm-8 Proposition">
+          <div class="container-fluid">
+<!-- première proposition -->
+            <div class="col-xs-4 col-md-4 col-lx-4 col-sm-4">
+                <div class="card boxProposition">
+                  <img src="damien.png" alt="John" class="logo">
+                  <h3 class="infoperso">Daminaminou !</h3>
+                  <p class="title">Autosuceur</p>
+                  <p>Clermont-ferrand</p>
+                  <p><button>Contacter !</button></p>
+                </div>
+            </div>
+<!-- Deuxième proposition -->
+            <div class="col-xs-4 col-md-4 col-lx-4 col-sm-4">
+                <div class="card boxProposition">
+                  <img src="damien.png" alt="John" class="logo">
+                  <h3 class="infoperso">Daminaminou !</h3>
+                  <p class="title">Autosuceur</p>
+                  <p>Clermont-ferrand</p>
+                  <p><button>Contacter !</button></p>
+                </div>
+            </div>
+<!-- Troisième proposition -->
+            <div class="col-xs-4 col-md-4 col-lx-4 col-sm-4">
+                <div class="card boxProposition">
+                  <img src="damien.png" alt="John" class="logo">
+                  <h3 class="infoperso">Daminaminou !</h3>
+                  <p class="title">Autosuceur</p>
+                  <p>Clermont-ferrand</p>
+                  <p><button>Contacter !</button></p>
+                </div>
+            </div>
+
+         </div>
+
+<!-- Partie pour rejoindre le forum -->
+            <div class="text-center boxForum">
+              <h3 class="infoperso">Pour pouvoir rencontrer de nouvelles personnes
+              qui font partie de la formation rejoignez le Forum !!</h3>
+              <a href="forum.php"><button type="button" class="btn btn-default" name="button">Rejoindre le forum de discussion</button></a>
+            </div>
+
+        </div>
+      </div>
+    </div>
+
+
+  </body>
+<!-- Pied de page -->
+  <div class="row text-center">
+    <ol class="breadcrumb piedpage">
+      <li class="breadcrumb-item"><a href="confidentialite.php">Condition d'utilisation</a></li>
+      <li class="breadcrumb-item"><a href="info_udev.php">Plus d'info</a></li>
+    </ol>
+  </div>
+</html>
